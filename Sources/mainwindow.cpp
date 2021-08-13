f70bb1ad9577853bcdf52c308d4c1ded1bc43cac
Frank Pereny <fjpereny@gmail.com>
2021-08-13 23:03:07 +0800

    Initial commit

---
+++ Sources/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "Headers/mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    this->close();
+}
